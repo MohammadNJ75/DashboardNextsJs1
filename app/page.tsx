b5ebbe4ca8ce58f0b72954ed076fd83f35f44e88
Nejati <mnejati766@gmail.com>
2024-09-20 12:26:53 +0330

    Last Update

--- app/page.tsx
+++ app/page.tsx
@@ -16,11 +16,11 @@ export default function Page() {
           <p
             className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}
           >
-            <strong>Welcome to Acme.</strong> This is the example for the{" "}
+            <strong>Welcome to Nejati Sample NextJS app.</strong> This is the example for the{" "}
             <a href="https://nextjs.org/learn/" className="text-blue-500">
-              Next.js Learn Course
+              employers
             </a>
-            , brought to you by Vercel.
+            , Made on Vercel Database
           </p>
           <Link
             href="/login"
